9e6da8ec2511141206979acf460c2dac9a1d15af
IJG7Momo <moritzschmieden@gmail.com>
2025-04-12 11:31:38 +0200

    Add files via upload

--- app.py
+++ app.py
@@ -1,8 +1,28 @@
 
-from flask import Flask, render_template
+from flask import Flask, render_template, request, redirect, url_for, session
 
 app = Flask(__name__)
+app.secret_key = "very_secret_key"
 
 @app.route("/")
-def home():
-    return render_template("dashboard.html")
+def index():
+    if "user" in session:
+        return redirect(url_for("dashboard"))
+    return render_template("index.html")
+
+@app.route("/login", methods=["POST"])
+def login():
+    username = request.form["username"]
+    session["user"] = username
+    return redirect(url_for("dashboard"))
+
+@app.route("/logout")
+def logout():
+    session.pop("user", None)
+    return redirect(url_for("index"))
+
+@app.route("/dashboard")
+def dashboard():
+    if "user" not in session:
+        return redirect(url_for("index"))
+    return render_template("dashboard.html", user=session["user"])
