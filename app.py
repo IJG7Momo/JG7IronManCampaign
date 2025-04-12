08c727334d0ee9f917eee220ccc4aeee0ff51ae4
IJG7Momo <moritzschmieden@gmail.com>
2025-04-12 11:16:09 +0200

    Add files via upload

--- app.py
+++ app.py
@@ -1,1 +1,8 @@
-# Flask app with login, user settings, admin logic
+
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+@app.route("/")
+def home():
+    return render_template("dashboard.html")
