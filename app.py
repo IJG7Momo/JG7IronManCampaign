4557c0b9af1c8ef6dc34b5685e94585b16f72194
IJG7Momo <moritzschmieden@gmail.com>
2025-04-12 09:04:11 +0200

    Add files via upload

--- app.py
+++ app.py
@@ -1,11 +1,10 @@
 
-from flask import Flask, render_template, request, redirect, session
+from flask import Flask, render_template
 app = Flask(__name__)
-app.secret_key = "geheim"
 
 @app.route("/")
 def home():
-    return "Willkommen im Geschwader-Logbuch!"
+    return render_template("index.html")
 
 if __name__ == "__main__":
     app.run(debug=True)
