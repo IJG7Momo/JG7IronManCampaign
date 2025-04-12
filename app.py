14619b1ad5385d77536adbfd0fe4a97c885b0920
IJG7Momo <moritzschmieden@gmail.com>
2025-04-12 17:57:02 +0200

    Add files via upload

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
-from flask import Flask, render_template, redirect, url_for, session
+from flask import Flask, render_template, redirect, url_for, session, request
+
 app = Flask(__name__)
 app.secret_key = 'supersecretkey'
 
@@ -6,12 +7,24 @@ app.secret_key = 'supersecretkey'
 def index():
     return redirect(url_for("login"))
 
-@app.route("/login")
+@app.route("/login", methods=["GET", "POST"])
 def login():
+    if request.method == "POST":
+        username = request.form["username"]
+        password = request.form["password"]
+        # TODO: Authentifizierung prüfen
+        session["user"] = username
+        return redirect(url_for("dashboard"))
     return render_template("login.html")
 
-@app.route("/register")
+@app.route("/register", methods=["GET", "POST"])
 def register():
+    if request.method == "POST":
+        username = request.form["username"]
+        email = request.form["email"]
+        password = request.form["password"]
+        # TODO: Registrierung speichern
+        return redirect(url_for("login"))
     return render_template("register.html")
 
 @app.route("/dashboard")
